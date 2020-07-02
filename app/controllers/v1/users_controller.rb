0dd0078eb52d5424c92bf2d1905d2a4f301091e5
masaki-michida <stupid763@gmail.com>
2020-07-02 20:59:04 +0900

    commit

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -2,12 +2,11 @@ class V1::UsersController < ApplicationController
   
   def index
     if params[:uid]
-      # find_byじゃなくていいのでは=uidはstringで定義していたので
-      current_user = User.find_by(uid: params[:uid])
-      render json: current_user
+      @user = User.find_by(uid: params[:uid])
+      render json: @user
     else
-      users = User.all
-      render json: users
+      @users = User.all
+      render json: @users
     end
   end
 
