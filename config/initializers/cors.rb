33088e246be3d614e29ca48d64f8c9d6485f15cc
masaki-michida <stupid763@gmail.com>
2020-07-05 01:45:02 +0900

    commit

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     if Rails.env.production?
-      origins 'https://sample-2046a.firebaseapp.com/'
+      origins 'https://sample-2046a.firebaseapp.com'
     else
       origins 'http://localhost:3000'
     end
