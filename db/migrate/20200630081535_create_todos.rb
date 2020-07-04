f323b42e1edcd321e0a12df4f692cb5be0f6811d
masaki-michida <stupid763@gmail.com>
2020-07-05 00:13:16 +0900

    schema

--- db/migrate/20200630081535_create_todos.rb
+++ db/migrate/20200630081535_create_todos.rb
@@ -2,7 +2,7 @@ class CreateTodos < ActiveRecord::Migration[5.2]
   def change
     create_table :todos do |t|
       t.string :title
-      t.references :user, foreign_key: true
+      t.references :user
 
       t.timestamps
     end
